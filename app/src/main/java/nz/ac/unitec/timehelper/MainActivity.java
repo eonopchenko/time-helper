cb3c63ec50fc8b0dadd6a3d511ba0112f779ca2d
eonopchenko <jake.spb@gmail.com>
2017-10-31 10:55:45 +1300

    ~Reverted DB replication

--- app/src/main/java/nz/ac/unitec/timehelper/MainActivity.java
+++ app/src/main/java/nz/ac/unitec/timehelper/MainActivity.java
@@ -2,7 +2,9 @@ package nz.ac.unitec.timehelper;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.ibm.bluemix.appid.android.api.AppID;
 import com.ibm.bluemix.appid.android.api.AuthorizationException;
@@ -11,7 +13,15 @@ import com.ibm.bluemix.appid.android.api.LoginWidget;
 import com.ibm.bluemix.appid.android.api.tokens.AccessToken;
 import com.ibm.bluemix.appid.android.api.tokens.IdentityToken;
 import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.Request;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.Response;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.ResponseListener;
 
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,30 +32,61 @@ public class MainActivity extends AppCompatActivity {
     private ClassAdapter mClassAdapter;
     private ListView lstClass;
 
+    private String mUserId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         activity = this;
         lstClass = (ListView) findViewById(R.id.lvClassList);
+        sClasses = new ClassModel(this.getApplicationContext());
 
         AppID.getInstance().initialize(getApplicationContext(), "382fc624-ad4d-461d-867c-99be7a1f2179", AppID.REGION_SYDNEY);
         Login(AppID.getInstance().getLoginWidget(), new LoginSuccessListener() {
             @Override
             public void onLoginSuccess(String userId) {
+
+                activity.mUserId = userId;
+
                 BMSClient.getInstance().initialize(getApplicationContext(), BMSClient.REGION_SYDNEY);
 
-                sClasses = new ClassModel(activity.getApplicationContext());
-                List<ClassItem> classes = sClasses.allClasses(userId);
-                activity.mClassAdapter = new ClassAdapter(activity, classes);
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        lstClass.setAdapter(activity.mClassAdapter);
+                /// Initialize put-request to Bluemix cloud
+                Request request = new Request(getApplicationContext().getResources().getString(R.string.cloudantUrl) + "/lecturer_timetable_db", Request.PUT);
+
+                /// Get auth string and convert to UTF-8
+                byte[] byteAuth = new byte[0];
+                String byteString = getString(R.string.cloudantUsername) + ":" + getString(R.string.cloudantPassword);
+                try {
+                    byteAuth = byteString.getBytes("UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                /// Convert auth string to Base64 scheme
+                String base64Auth = Base64.encodeToString(byteAuth, Base64.NO_WRAP);
+
+                /// Set request headers
+                HashMap headers = new HashMap();
+                List<String> authorization = new ArrayList<>();
+                authorization.add("Basic " + base64Auth);
+                headers.put("authorization", authorization);
+                request.setHeaders(headers);
+
+                /// Send request
+                request.send(getApplicationContext(), new ResponseListener() {
+                    @Override
+                    public void onSuccess(Response response) {
+                    }
+
+                    @Override
+                    public void onFailure(Response response, Throwable throwable, JSONObject extendedInfo) {
                     }
                 });
-                MapFragment frMap = (MapFragment) getFragmentManager().findFragmentById(R.id.frMap);
-                frMap.onClassLocationAvailable(classes, activity);
 
+                sClasses.setReplicationListener(activity);
+
+                reloadClassesFromModel(userId);
             }
         });
     }
@@ -76,5 +117,29 @@ public class MainActivity extends AppCompatActivity {
         if(sClasses != null) {
             sClasses.startPullReplication();
         }
+    }
+
+    private void reloadClassesFromModel(final String userId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                List<ClassItem> classes = sClasses.allClasses(userId);
+                activity.mClassAdapter = new ClassAdapter(activity, classes);
+                lstClass.setAdapter(activity.mClassAdapter);
+                MapFragment frMap = (MapFragment) getFragmentManager().findFragmentById(R.id.frMap);
+                frMap.onClassLocationAvailable(classes, activity);
+            }
+        });
+    }
+
+    void replicationComplete() {
+        reloadClassesFromModel(mUserId);
+        Toast.makeText(getApplicationContext(),
+                R.string.replication_completed,
+                Toast.LENGTH_LONG).show();
+    }
+
+    void replicationError() {
+        reloadClassesFromModel(mUserId);
     }
 }
